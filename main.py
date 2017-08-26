d968b8ce65dc83628915b4d5cce686bdfe206def
chio <schiob4@gmail.com>
2017-08-25 19:26:17 -0500

    fix sys.argv

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 import webbrowser
 import sys
 
 def main():
     URL = 'http://cataas.com/cat/gif/says/'
-    webbrowser.open('{}{}'.format(URL, sys.argv[1]))
+    if len(sys.argv) > 1:
+        webbrowser.open('{}{}'.format(URL, ' '.join(sys.argv[1:])))
+    else:
+        webbrowser.open('{}{}'.format(URL, 'holi'))
 
 # Si el archivo es el hilo principal que se ejecute si no pos no juejuejue
 if __name__ == '__main__':
